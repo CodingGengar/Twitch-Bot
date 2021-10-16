47e95e410310f324c53762c20b38533b96b33a20
snug <68132950+snugdev@users.noreply.github.com>
2021-10-16 01:02:29 +0000

    added package.json files

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "twitch",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "dotenv": "^10.0.0",
+    "nodemon": "^2.0.13",
+    "obs-websocket-js": "^4.0.3",
+    "tmi.js": "^1.5.0",
+    "youtube-api": "^3.0.1"
+  }
+}
